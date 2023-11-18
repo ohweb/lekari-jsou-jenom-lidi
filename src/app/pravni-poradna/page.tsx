9c75cb67ea3f2fe06556c39ac822e9e9304f37c2
Ondra Hlaváč <ondra@hlavac.cz>
2023-11-18 03:11:53 +0100

    feat: doplnění textů od právníka

--- src/app/pravni-poradna/page.tsx
+++ src/app/pravni-poradna/page.tsx
@@ -217,6 +217,54 @@ zaměstnanec byl uznán dočasně práce neschopným nebo z důvodu čerpání m
 rodičovské dovolené, je zaměstnavatel povinen určit dobu čerpání této dovolené po skončení těchto
 překážek v práci.
                     </p>
+
+                    <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+                        Můj nadřízený mi sdělil, že jsem nevykonal tento rok ještě žádnou práci přesčas, jelikož se v naší nemocnici slouží na DPČ. V prosinci mi proto plánuje nařizovat práci přesčas do zákonného limitu 150 hodin. Jak se mám bránit?
+                    </h2>
+                    <p className='mt-6'>
+                        Uvedená situace je v praxi v poslední době velice častá. Výchozí podmínky jsou takové, že lékař, který má vedle své pracovní smlouvy u totožného zaměstnavatele uzavřenu ještě dohodu o pracovní činnosti (DPČ), na jejímž základě vykonává zcela totožnou práci jako dle pracovní smlouvy, tuto DPČ vypoví a zaměstnavatel tomuto lékaři obratem nařídí práci přesčas (do 150 hodin ročně) s nepravdivým odůvodněním, že tuto práci přesčas může do limitu 150 hodin nařídit, protože práce na vypovězenou DPČ přece nebyla prací přesčas.
+                    </p>
+                    <p className='mt-6'>
+                        K této argumentaci zaměstnavatele je nutno především konstatovat, že i dle judikatury Nejvyššího soudu ČR a Nejvyššího správního soudu ČR (viz rozhodnutí Nejvyššího soudu ČR sp. zn. 21 Cdo 1573/2012 či Nejvyššího správního soudu č.j. 3 Ads 11/2011 – 47) je DPČ, na jejímž základě má být vykonávána práce stejného druhu jako je práce, kterou zaměstnanec (lékař) již vykonává na základě pracovní smlouvy (jakkoliv je v DPČ tato práce účelově pojmenovaná jinak), od počátku neplatná, neboť takováto konstrukce je v zásadním rozporu s ustanovením § 34 odst. 2 věta prvá zákoníku práce, kde se doslova uvádí:
+                        <span className="italic">
+                            Zaměstnanec v dalším základním pracovněprávním vztahu u téhož zaměstnavatele nesmí vykonávat práce, které jsou stejně druhově vymezeny.
+                        </span>
+                    </p>
+                    <p className='mt-6'>
+                        Pokud jde o posouzení právní povahy práce dle takto neplatné DPČ, použije se dikce ustanovení § 555 občanského zákoníku, kde se uvádí:
+                    </p>
+                        <ul role="list">
+                        <li>(1) Právní jednání se posuzuje podle svého obsahu.</li>
+                        <li>(2) Má-li být určitým právním jednáním zastřeno jiné právní jednání, posoudí se podle jeho pravé povahy.</li>
+                        </ul>
+                    <p className='mt-6'>
+                        V případě jakýchkoliv pochybností je dále možné se odkázat na ustanovení § 18 zákoníku práce, dle nějž platí, že je-li možné právní jednání vyložit různým způsobem, použije se výklad pro zaměstnance nejpříznivější.
+                    </p>
+                    <p className='mt-6'>
+                        V intencích citovaných zákonných ustanovení platí, že práce vykonávaná na základě neplatné DPČ je ve vztahu k pracovní smlouvě prací nad stanovenou týdenní pracovní dobu, tedy prací odpovídající zákonné definici práce přesčas dle § 78 odst. 1 písm. i) zákoníku práce.
+                    </p>
+                    <p className='mt-6'>
+                        Z uvedeného výkladu tedy jednoznačně plyne, že veškerá práce vykonávaná za výše uvedených podmínek na základě neplatné DPČ je prací přesčas. Pokud tedy práce dle DPČ v daném kalendářním roce překročila 150 hodin, není zaměstnavatel oprávněn dotčenému zaměstnanci jakoukoli další práci přesčas nařizovat. Naopak zaměstnanec má právo požadovat kompenzaci rozdílu mezi odměnou z DPČ a tím, co mu měl zaměstnavatel vyplatit, pokud by tato práce byla správně hodnocena jako práce přesčas.
+                    </p>
+                    <p className='mt-6'>
+                        Jen pro úplnost uvádím, že u nařízené práce přesčas do 150 hodin ročně platí omezení dle ustanovení § 93 odst. 2 zákoníku práce, tedy pravidlo, že nařízená práce přesčas nesmí u zaměstnance činit více než 8 hodin v jednotlivých týdnech. Na rozdíl od dohodnuté přesčasové práce, kde se vychází z týdenního průměru (a kterou je tak možné sloužit v jednom týdnu např. 12 hodin a v druhém týdnu jen 4 hodiny, tedy průměrně 8 hodin přesčasů týdně) u nařízené práce přesčas do 150 hodin ročně může zaměstnanec sloužit nejvíce 8 hodin přesčasů týdně.
+                    </p>
+                    <p className='mt-6'>
+                        Zaměstnavateli tedy doporučuji v případě jeho postupu, kdy s ohledem na ukončení DPČ lékaři v prosinci nařídí práci přesčas zaslat toto sdělení (které je samozřejmě třeba upravit dle konkrétních okolností):
+                    </p>
+                    <p className='mt-6'>
+                        Vážený pane řediteli,
+
+                        tímto Vám sděluji, že dohoda o pracovní činnosti (DPČ), na jejímž základě jsem v tomto roce vykonával fakticky stejnou práci jako dle mé aktuální pracovní smlouvy, byla od počátku neplatná, když jejím účelem bylo pouze zastřít výkon práce přesčas (blíže viz § 34 odst. 2 věta prvá zákoníku práce a § 555 odst. 2 občanského zákoníku, jakož i rozhodnutí Nejvyššího soudu ČR sp. zn. 21 Cdo 1573/2012 či Nejvyššího správního soudu č.j. 3 Ads 11/2011 – 47).
+
+                        Veškerá práce odvedená na základě uvedené dohody o pracovní činnosti v tomto roce tak byla prací přesčas. Vzhledem k tomu, že práce na předmětnou DPČ překročila v tomto kalendářním roce limity nařízené práce přesčas (do 150 hodin ročně), jakož i limity dohodnuté práce přesčas (do 416 hodin ročně včetně nařízených 150 hodin), byl by jakýkoliv další výkon nařízené či dohodnuté práce přesčas v tomto roce v rozporu se zákoníkem práce (dohodu o další dohodnuté práci přesčas ve zdravotnictví jsem s Vámi neuzavřel).
+
+                        Bude-li mi tedy práce přesčas v prosinci nařízena, nemohu tuto legálně vykonávat. Naopak bych byl v takovém případě nucen její výkon odmítnout a v případě jakéhokoliv nátlaku či postihu za toto odmítnutí se následně obrátit na místně příslušný inspektorát práce s podnětem k prověření takového Vašeho postupu, který však samozřejmě nechci předjímat.
+
+                        V této souvislosti Vás též upozorňuji na nedoplatek mé odměny za práci dle DPČ, když tato měla být správně odměňována jako práce přesčas a odměna za tuto práci měla být zohledněna i v průměrném výdělku dle pracovní smlouvy, k čemuž nedošlo a čímž jsem byl na odměně značně zkrácen (viz přiložený článek).
+
+                        S pozdravem…
+                    </p>
 </div></div>
     
         </>
