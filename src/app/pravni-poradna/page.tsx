1c581959cc8ce776988ba1ef4e5ec535076ffdd2
Ondra Hlaváč <ondra@hlavac.cz>
2023-11-22 19:34:52 +0100

    feat(content): clean up the legal section of the website visually

--- src/app/pravni-poradna/page.tsx
+++ src/app/pravni-poradna/page.tsx
@@ -1,35 +1,37 @@
-import {type Metadata} from 'next'
-import LegalFaq from "@/components/LegalFaqs/LegalFaq";
-import getLegalFaqs from "@/components/LegalFaqs/getLegalFaqs";
+import { type Metadata } from 'next'
+import LegalFaq from '@/components/LegalFaqs/LegalFaq'
+import getLegalFaqs from '@/components/LegalFaqs/getLegalFaqs'
 
 export const metadata: Metadata = {
-    title: 'Právní poradna',
+  title: 'Právní poradna',
 }
 
-
 export default function PravniPoradna() {
-    const legalFaqs = getLegalFaqs()
-    return (
-        <>
-            <div className="bg-white px-6 py-32 lg:px-8">
-                <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        Nejčastější otázky k nepřetržitému režimu práce
-                    </h1>
-                    <p className="mt-6 text-xl leading-8">
-                        Ve spolupráci s Mgr. Alešem Buriánkem jsme pro vás
-                        připravili nejčastější právní otázky a odpovědi.
-                    </p>
-                    <dl className="mt-10 space-y-12 divide-y divide-gray-900/10">
-                        {legalFaqs.map((oneLegalFaq) => (
-                            <LegalFaq question={oneLegalFaq.data.question}
-                                      answer={oneLegalFaq.content}
-                                      key={oneLegalFaq.data.id}/>
-                        ))}
-                    </dl>
-                </div>
-            </div>
-
-        </>
-    )
+  const legalFaqs = getLegalFaqs()
+  return (
+    <>
+      <div className="bg-white px-6 py-32 lg:px-8">
+        <div className="mx-auto max-w-3xl divide-y divide-gray-900/10 text-base leading-7 text-gray-700">
+          <div className="mx-auto max-w-2xl md:text-center">
+            <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
+              Nejčastější otázky k nepřetržitému režimu práce
+            </h2>
+            <p className="mt-4 text-lg tracking-tight text-slate-700">
+              Ve spolupráci s Mgr. Alešem Buriánkem jsme pro vás připravili
+              nejčastější právní otázky a odpovědi.
+            </p>
+          </div>
+          <dl className="mt-16 space-y-12 divide-y divide-gray-900/10">
+            {legalFaqs.map((oneLegalFaq) => (
+              <LegalFaq
+                question={oneLegalFaq.data.question}
+                answer={oneLegalFaq.content}
+                key={oneLegalFaq.data.id}
+              />
+            ))}
+          </dl>
+        </div>
+      </div>
+    </>
+  )
 }
--- src/components/LegalFaqs/LegalFaq.tsx
+++ src/components/LegalFaqs/LegalFaq.tsx
@@ -1,37 +1,42 @@
-"use client"
-import {Disclosure} from "@headlessui/react";
-import {MinusSmallIcon, PlusSmallIcon} from "@heroicons/react/24/outline";
-import Markdown from "markdown-to-jsx";
+'use client'
+import { Disclosure } from '@headlessui/react'
+import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline'
+import Markdown from 'markdown-to-jsx'
 
 export interface LegalFaqProps {
-    question: string
-    answer: string
+  question: string
+  answer: string
 }
-const LegalFaq = ({question, answer}: LegalFaqProps) => {
-    return (
-        <Disclosure as="div" className="pt-6">
-            {({ open }) => (
-                <>
-                    <dt>
-                        <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
-                            <span className="text-2xl font-bold tracking-tight leading-7">{question}</span>
-                            <span className="ml-6 flex h-7 items-center">
-                          {open ? (
-                              <MinusSmallIcon className="h-6 w-6" aria-hidden="true" />
-                          ) : (
-                              <PlusSmallIcon className="h-6 w-6" aria-hidden="true" />
-                          )}
-                        </span>
-                        </Disclosure.Button>
-                    </dt>
-                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                        <div>
-                            <Markdown className="prose lg:prose-xl text-base leading-7 text-gray-600 text-justify">{answer}</Markdown>
-                        </div>
-                    </Disclosure.Panel>
-                </>
-            )}
-        </Disclosure>    )
+const LegalFaq = ({ question, answer }: LegalFaqProps) => {
+  return (
+    <Disclosure as="div" className="pt-6">
+      {({ open }) => (
+        <>
+          <dt>
+            <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
+              <span className="mb-6 text-xl font-bold leading-7 tracking-tight">
+                {question}
+              </span>
+              <span className="ml-6 flex h-7 items-center">
+                {open ? (
+                  <MinusSmallIcon className="h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <PlusSmallIcon className="h-6 w-6" aria-hidden="true" />
+                )}
+              </span>
+            </Disclosure.Button>
+          </dt>
+          <Disclosure.Panel as="dd" className="mt-2 pr-12">
+            <div>
+              <Markdown className="prose text-justify text-base leading-7 text-gray-600 lg:prose-xl">
+                {answer}
+              </Markdown>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
 }
 
 export default LegalFaq
