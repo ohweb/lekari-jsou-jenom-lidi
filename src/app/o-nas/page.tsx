3585e1f3571c9865d5a3253c04aeb976f1190784
Ondra Hlaváč <ondra@hlavac.cz>
2024-02-24 15:36:37 +0100

    content: update sekce o nas

--- src/app/o-nas/page.tsx
+++ src/app/o-nas/page.tsx
@@ -9,7 +9,7 @@ import tymPitrova from '@/images/nas-tym/tym-pitrova-hana.jpg'
 import tymSvobodova from '@/images/nas-tym/tym-svobodova-marie.jpg'
 import tymDunajova from '@/images/nas-tym/tym-dunajova-klara.jpg'
 import tymSplino from '@/images/nas-tym/tym-splino-filip.jpg'
-import Image from "next/image";
+import Image from 'next/image'
 
 export const metadata: Metadata = {
   title: 'O nás',
@@ -49,66 +49,76 @@ const values = [
 ]
 
 const images = [
-  {image: tymMyval },
-  {image: tymPrada },
-  {image: tymPavelka },
-  {image: tymHilserova },
-  {image: tymPitrova },
-  {image: tymSvobodova },
-  {image: tymDunajova },
-  {image: tymSplino },
+  { image: tymMyval },
+  { image: tymHilserova },
+  { image: tymPavelka },
+  { image: tymPrada },
+  { image: tymSvobodova },
+  { image: tymPitrova },
+  { image: tymDunajova },
+  { image: tymSplino },
 ]
 
 export default function ONas() {
-    return(
-        <>
-
-        {/* Values section */}
-        <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Náš tým</h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-            </p>
-          </div>
-          <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-4 gap-y-8 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-4">
-            {images.map((value) => (
-                <Image src={value.image} alt="člen týmu" key={value.image.src} />
-            ))}
-          </dl>
+  return (
+    <>
+      {/* Values section */}
+      <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Náš tým
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600"></p>
         </div>
+        <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-4 gap-y-8 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+          {images.map((value) => (
+            <Image src={value.image} alt="člen týmu" key={value.image.src} />
+          ))}
+        </dl>
+      </div>
 
-        {/* Logo cloud */}
-        <div className="relative isolate -z-10 mt-32 sm:mt-48">
-          <div className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]">
-            <svg className="h-[40rem] w-[80rem] flex-none stroke-gray-200" aria-hidden="true">
-              <defs>
-                <pattern
-                  id="e9033f3e-f665-41a6-84ef-756f6778e6fe"
-                  width={200}
-                  height={200}
-                  x="50%"
-                  y="50%"
-                  patternUnits="userSpaceOnUse"
-                  patternTransform="translate(-100 0)"
-                >
-                  <path d="M.5 200V.5H200" fill="none" />
-                </pattern>
-              </defs>
-              <svg x="50%" y="50%" className="overflow-visible fill-gray-50">
-                <path d="M-300 0h201v201h-201Z M300 200h201v201h-201Z" strokeWidth={0} />
-              </svg>
-              <rect width="100%" height="100%" strokeWidth={0} fill="url(#e9033f3e-f665-41a6-84ef-756f6778e6fe)" />
+      {/* Logo cloud */}
+      <div className="relative isolate -z-10 mt-32 sm:mt-48">
+        <div className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]">
+          <svg
+            className="h-[40rem] w-[80rem] flex-none stroke-gray-200"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="e9033f3e-f665-41a6-84ef-756f6778e6fe"
+                width={200}
+                height={200}
+                x="50%"
+                y="50%"
+                patternUnits="userSpaceOnUse"
+                patternTransform="translate(-100 0)"
+              >
+                <path d="M.5 200V.5H200" fill="none" />
+              </pattern>
+            </defs>
+            <svg x="50%" y="50%" className="overflow-visible fill-gray-50">
+              <path
+                d="M-300 0h201v201h-201Z M300 200h201v201h-201Z"
+                strokeWidth={0}
+              />
             </svg>
-          </div>
-
-
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
-              Podpořili nás
-            </h2>
-            <Partners/>
-          </div>
+            <rect
+              width="100%"
+              height="100%"
+              strokeWidth={0}
+              fill="url(#e9033f3e-f665-41a6-84ef-756f6778e6fe)"
+            />
+          </svg>
         </div>
-        </>
-    )
+
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+            Podpořili nás
+          </h2>
+          <Partners />
+        </div>
+      </div>
+    </>
+  )
 }
